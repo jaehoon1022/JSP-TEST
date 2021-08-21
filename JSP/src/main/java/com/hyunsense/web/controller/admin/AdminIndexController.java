2d03a08a0c535f6d88b8d08e8d03c7aacb37e9b3
jaehoon1022 <hyunsense1022@gmail.com>
2021-08-21 17:23:45 +0900

    implements adminController, adminIndexPage

---
+++ JSP/src/main/java/com/hyunsense/web/controller/admin/AdminIndexController.java
@@ -0,0 +1,21 @@
+package com.hyunsense.web.controller.admin;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/index")
+public class AdminIndexController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/admin/index.jsp");
+        dispatcher.forward(request,response);
+    }
+}
